24bac690586119c0edd103aaea82ee3a254874f4
wangxingang <wangxgmail@163.com>
2023-10-12 00:38:22 +0800

    全局异常处理器

---
+++ src/main/java/com/itheima/reggie/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.itheima.reggie.exception;
+
+import com.itheima.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * 前面的程序还存在一个问题，就是当我们在新增员工时输入的账号已经存在，由于employee表中对该字段加入了唯一约束，此时程序会抛出异常:
+     * java.sql.SQLIntegrityConstraintViolationException: Duplicate entry 'heniang' for key 'idx_username'
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> SQLIntegrityConstraintViolationExceptionHandler(SQLIntegrityConstraintViolationException e) {
+        log.error("{}:{}", e.getClass().getName(), e.getMessage());
+
+        if(e.getMessage().contains("Duplicate entry")){
+            String[] split = e.getMessage().split(" ");
+            String msg=split[2]+"已存在";
+            return R.error(msg);
+        }
+
+        return R.error("未知错误");
+    }
+}
