5a1a9d511ee92ba8356380a702a1fa4757fa5784
wangxingang <wangxgmail@163.com>
2023-10-12 00:34:23 +0800

    新增员工

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -63,5 +64,32 @@ public class EmployeeController {
         // 清理 Session 中保存的当前员工登录的 id
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
+    }
+
+
+    /**
+     * 新增员工
+     * @param request  获取当前登录用户
+     * @param employee
+     * @return
+     */
+    @PostMapping
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("新增员工，员工信息：{}", employee.toString());
+
+        // 设置初始密码，需要进行md5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        // 从 Session 中获取当前登录用户（即创建人和更新人）
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+
+        return R.success("新增员工成功");
     }
 }
