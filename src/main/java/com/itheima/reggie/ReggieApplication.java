b1165222efb823b0348493426ca1923d7e9641a6
wangxingang <wangxgmail@163.com>
2023-10-12 00:28:16 +0800

    Filter 实现拦截页面访问

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -3,9 +3,16 @@ package com.itheima.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+/**
+ * 在SpringBootApplication上使用 @ServletComponentScan 注解后，
+ * Servlet（控制器）、Filter（过滤器）、Listener（监听器）可以直接通过@WebServlet、@WebFilter、@WebListener注解自动注册到Spring容器中，无需其他代码。
+ */
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieApplication {
 
 	public static void main(String[] args) {
