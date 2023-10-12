e993d132f8b7dc40b75acfc0b3e90204dbc21d52
wangxingang <wangxgmail@163.com>
2023-10-12 23:43:34 +0800

    新增菜品

---
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -0,0 +1,27 @@
+package com.itheima.reggie.controller;
+
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.DishDto;
+import com.itheima.reggie.service.DishFlavorService;
+import com.itheima.reggie.service.DishService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dish")
+public class DishController {
+    @Autowired
+    private DishService dishService;
+    @Autowired
+    private DishFlavorService dishFlavorService;
+
+
+    @PostMapping
+    public R<String> save(@RequestBody DishDto dishDto){
+        dishService.saveWithFlavor(dishDto);
+        return R.success("新增菜品成功");
+    }
+}
